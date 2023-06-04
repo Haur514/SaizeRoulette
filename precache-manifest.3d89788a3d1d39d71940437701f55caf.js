523f5b6183752b0ef0ec647423117389944dd4cd
Haur514 <haruka.yoshioka.514@gmail.com>
2023-06-04 20:51:40 +0900

    Updates

---
+++ precache-manifest.3d89788a3d1d39d71940437701f55caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba6e1e939083eaffd15a",
+    "url": "/SaizeRoulette/static/css/main.8fb7e75e.chunk.css"
+  },
+  {
+    "revision": "ba6e1e939083eaffd15a",
+    "url": "/SaizeRoulette/static/js/main.ba6e1e93.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/SaizeRoulette/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "d38d4effbe7c0e204576",
+    "url": "/SaizeRoulette/static/js/2.d38d4eff.chunk.js"
+  },
+  {
+    "revision": "d9475b8cc88aaa538a61",
+    "url": "/SaizeRoulette/static/js/runtime~main.d9475b8c.js"
+  },
+  {
+    "revision": "6cd9e5060d943f7bf48946b2a6cedea3",
+    "url": "/SaizeRoulette/static/media/logo.6cd9e506.png"
+  },
+  {
+    "revision": "a2608e32b695750bf4fddba6e3b3da73",
+    "url": "/SaizeRoulette/index.html"
+  }
+];
